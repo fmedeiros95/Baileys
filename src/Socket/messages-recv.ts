02227ed010fb99ef79aa9b80e1bbe8bc07902b20
Felipe Medeiros <medeiros.dev@gmail.com>
2024-10-07 00:24:43 -0300

    feat: Adicionar função de cancelar chamada
    A função `terminateCall` foi renomeada para `cancelCall` para melhor refletir sua finalidade de cancelar uma chamada. Além disso, foram removidos trechos de código relacionados à configuração de vídeo na função `offerCall`.

--- src/Socket/messages-recv.ts
+++ src/Socket/messages-recv.ts
@@ -122,27 +122,12 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 		await sendNode(stanza)
 	}
 
-	const offerCall = async(toJid: string, isVideo = false) => {
+	const offerCall = async(toJid: string) => {
 		const callId = randomBytes(16).toString('hex').toUpperCase().substring(0, 64)
 
 		const offerContent: BinaryNode[] = []
 		offerContent.push({ tag: 'audio', attrs: { enc: 'opus', rate: '16000' }, content: undefined })
 		offerContent.push({ tag: 'audio', attrs: { enc: 'opus', rate: '8000' }, content: undefined })
-
-		if(isVideo) {
-			offerContent.push({
-				tag: 'video',
-				attrs: {
-					orientation: '0',
-					'screen_width': '1920',
-					'screen_height': '1080',
-					'device_orientation': '0',
-					enc: 'vp8',
-					dec: 'vp8',
-				}
-			})
-		}
-
 		offerContent.push({ tag: 'net', attrs: { medium: '3' }, content: undefined })
 		offerContent.push({ tag: 'capability', attrs: { ver: '1' }, content: new Uint8Array([1, 4, 255, 131, 207, 4]) })
 		offerContent.push({ tag: 'encopt', attrs: { keygen: '2' }, content: undefined })
@@ -186,12 +171,11 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 		await query(stanza)
 		return {
 			id: callId,
-			to: toJid,
-			isVideo,
+			to: toJid
 		}
 	}
 
-	const terminateCall = async(callId: string, callTo: string) => {
+	const cancelCall = async(callId: string, callTo: string) => {
 		const stanza: BinaryNode = ({
 			tag: 'call',
 			attrs: {
@@ -199,15 +183,17 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 				to: callTo,
 			},
 			content: [{
-				tag: 'terminate',
+				tag: 'cancel',
 				attrs: {
 					'call-id': callId,
 					'call-creator': authState.creds.me!.id,
+					count: '0',
 				},
 				content: undefined,
 			}],
 		})
-		await query(stanza)
+		const aa = await query(stanza)
+		console.log(aa)
 	}
 
 	const rejectCall = async(callId: string, callFrom: string) => {
@@ -1132,7 +1118,7 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 		sendRetryRequest,
 		offerCall,
 		rejectCall,
-		terminateCall,
+		cancelCall,
 		fetchMessageHistory,
 		requestPlaceholderResend,
 	}
