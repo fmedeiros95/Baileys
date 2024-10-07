dbbdcd84a348395a2cd5cc199e21ea1306fb48d9
Felipe Medeiros <medeiros.dev@gmail.com>
2024-10-06 23:11:48 -0300

    feat: add offerCall function for initiating calls

--- src/Socket/messages-recv.ts
+++ src/Socket/messages-recv.ts
@@ -37,6 +37,7 @@ import {
 	isJidGroup, isJidStatusBroadcast,
 	isJidUser,
 	jidDecode,
+	jidEncode,
 	jidNormalizedUser,
 	S_WHATSAPP_NET
 } from '../WABinary'
@@ -68,6 +69,8 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 		sendReceipt,
 		uploadPreKeys,
 		sendPeerDataOperationMessage,
+		createParticipantNodes,
+		getUSyncDevices,
 	} = sock
 
 	/** this mutex ensures that each retryRequest will wait for the previous one to finish */
@@ -117,6 +120,75 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 
 		logger.debug({ recv: { tag, attrs }, sent: stanza.attrs }, 'sent ack')
 		await sendNode(stanza)
+	}
+
+	const offerCall = async(toJid: string, isVideo = false) => {
+		const callId = randomBytes(16).toString('hex').toUpperCase().substring(0, 64)
+
+		const offerContent: BinaryNode[] = []
+		offerContent.push({ tag: 'audio', attrs: { enc: 'opus', rate: '16000' }, content: undefined })
+		offerContent.push({ tag: 'audio', attrs: { enc: 'opus', rate: '8000' }, content: undefined })
+
+		if(isVideo) {
+			offerContent.push({
+				tag: 'video',
+				attrs: {
+					orientation: '0',
+					'screen_width': '1920',
+					'screen_height': '1080',
+					'device_orientation': '0',
+					enc: 'vp8',
+					dec: 'vp8',
+				}
+			})
+		}
+
+		offerContent.push({ tag: 'net', attrs: { medium: '3' }, content: undefined })
+		offerContent.push({ tag: 'capability', attrs: { ver: '1' }, content: new Uint8Array([1, 4, 255, 131, 207, 4]) })
+		offerContent.push({ tag: 'encopt', attrs: { keygen: '2' }, content: undefined })
+
+		const encKey = randomBytes(32)
+
+		const devices = (await getUSyncDevices([toJid], true, false)).map(({ user, device }) => jidEncode(user, 's.whatsapp.net', device))
+
+		await assertSessions(devices, true)
+
+		const { nodes: destinations, shouldIncludeDeviceIdentity } = await createParticipantNodes(devices, {
+			call: {
+				callKey: encKey
+			}
+		})
+
+		offerContent.push({ tag: 'destination', attrs: {}, content: destinations })
+
+		if(shouldIncludeDeviceIdentity) {
+			offerContent.push({
+				tag: 'device-identity',
+				attrs: {},
+				content: encodeSignedDeviceIdentity(authState.creds.account!, true)
+			})
+		}
+
+		const stanza: BinaryNode = ({
+			tag: 'call',
+			attrs: {
+				to: toJid,
+			},
+			content: [{
+				tag: 'offer',
+				attrs: {
+					'call-id': callId,
+					'call-creator': authState.creds.me!.id,
+				},
+				content: offerContent,
+			}],
+		})
+		await query(stanza)
+		return {
+			callId,
+			toJid,
+			isVideo,
+		}
 	}
 
 	const rejectCall = async(callId: string, callFrom: string) => {
@@ -1039,6 +1111,7 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 		...sock,
 		sendMessageAck,
 		sendRetryRequest,
+		offerCall,
 		rejectCall,
 		fetchMessageHistory,
 		requestPlaceholderResend,
